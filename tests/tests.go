960b6a8ad84060154d7d4ef1fa85c962b87aee27
Reiner Hüchting <reiner.huechting@gmx.de>
2021-11-21 22:34:07 +0100

    Test eingebaut, der die Diagonalen ausgibt und so die Hauptdiagonalfunktionen testet.

--- tests/tests.go
+++ tests/tests.go
@@ -12,11 +12,25 @@ func main() {
 		board := b.MakeBoard(3, " ")
 		b.PrintBoard(board)
 		fmt.Println()
+		fmt.Println()
+	}
+	{
+		fmt.Println("Hauptdiagonalen des TicTacToe-Felds besetzen, Spielfeld und Diagonalen ausgeben.")
+		board := b.MakeBoard(3, " ")
+		board[0][0] = "X"
+		board[1][1] = "X"
+		board[2][2] = "X"
+		b.PrintBoard(board)
+		fmt.Printf("Diagonale 1: %v\n", b.GetPrincipalDiagonal1(board))
+		fmt.Printf("Diagonale 2: %v\n", b.GetPrincipalDiagonal2(board))
+		fmt.Println()
+		fmt.Println()
 	}
 	{
 		fmt.Println("Leeres Schachbrett (Größe 8x8):")
 		board := b.MakeBoard(8, " ")
 		b.PrintBoard(board)
+		fmt.Println()
 		fmt.Println()
 	}
 }
