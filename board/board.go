798a443b2a161baaefeabc7d3911b8937bfc7847
Reiner Hüchting <reiner.huechting@gmx.de>
2021-11-21 21:07:45 +0100

    Weitere alllgemeine Spielfeld-Funktionen in das board-Package verschoben.

--- board/board.go
+++ board/board.go
@@ -1,5 +1,7 @@
 package board
 
+import "fmt"
+
 func MakeBoard(size int, initChar string) [][]string {
 	// Definieren einer 2D-Slice
 	var board [][]string
@@ -12,4 +14,55 @@ func MakeBoard(size int, initChar string) [][]string {
 		board = append(board, row)
 	}
 	return board
+}
+
+// Das Spielfeld mit Trennlinien ausgeben.
+func PrintBoard(board [][]string) {
+	for i, row := range board {
+		printRow(row)
+		if i < len(board)-1 {
+			fmt.Println("---+---+---")
+		}
+	}
+}
+
+// Eine Zeile des Spielfelds mit Trennzeichen ausgeben.
+func printRow(row []string) {
+	for j := 0; j < len(row); j++ {
+		fmt.Print(" " + row[j] + " ")
+		if j < len(row)-1 {
+			fmt.Print("|")
+		}
+	}
+	fmt.Println()
+}
+
+// Liefert die i-te Spalte des Spielfelds als Liste.
+func GetColumn(board [][]string, i int) []string {
+	var result []string
+	for _, row := range board {
+		result = append(result, row[i])
+	}
+	return result
+}
+
+// Liefert die Diagonale von links oben nach rechts unten.
+func GetDiagonal1(board [][]string) []string {
+	var result []string
+	for i, _ := range board {
+		result = append(result, board[i][i])
+	}
+
+	return result
+}
+
+// Liefert die Diagonale von rechts oben nach links unten.
+func GetDiagonal2(board [][]string) []string {
+	var result []string
+	l := len(board) - 1
+	for i, _ := range board {
+		result = append(result, board[i][l-i])
+	}
+
+	return result
 }
--- tictactoe/spielfeld.go
+++ tictactoe/spielfeld.go
@@ -39,7 +39,7 @@ func mainLoop(board [][]string) {
 // Fragt den Spieler mit dem Zeichen 'char' nach seinem Zug
 // (z.B. Zeile und Spalte) und führt ihn aus.
 func move(board [][]string, char string) {
-	printBoard(board)
+	b.PrintBoard(board)
 	fmt.Printf("Spieler %v, du bist dran.\n", char)
 
 	var row, col int
@@ -65,27 +65,6 @@ func move(board [][]string, char string) {
 	board[row][col] = char
 }
 
-// Das Spielfeld mit Trennlinien ausgeben.
-func printBoard(board [][]string) {
-	for i, row := range board {
-		printRow(row)
-		if i < len(board)-1 {
-			fmt.Println("---+---+---")
-		}
-	}
-}
-
-// Eine Zeile des Spielfelds mit Trennzeichen ausgeben.
-func printRow(row []string) {
-	for j := 0; j < len(row); j++ {
-		fmt.Print(" " + row[j] + " ")
-		if j < len(row)-1 {
-			fmt.Print("|")
-		}
-	}
-	fmt.Println()
-}
-
 // Nimmt das Board als Parameter und liefert true, wenn
 // es in einer Zeile drei mal 'char' enthält.
 func checkRows(board [][]string, char string) bool {
@@ -108,55 +87,25 @@ func checkList(row []string, char string) bool {
 	return true
 }
 
-// Liefert die i-te Spalte des Spielfelds als Liste.
-func getColumn(board [][]string, i int) []string {
-	var result []string
-	for _, row := range board {
-		result = append(result, row[i])
-	}
-	return result
-}
-
 // Nimmt das Board als Parameter und liefert true, wenn
 // es in einer Spalte drei mal 'char' enthält.
 // Funktioniert erstmal nur für quadratische Spielfelder.
 func checkColumns(board [][]string, char string) bool {
 	for i, _ := range board {
-		if checkList(getColumn(board, i), char) {
+		if checkList(b.GetColumn(board, i), char) {
 			return true // Early Out
 		}
 	}
 	return false
 }
 
-// Liefert die Diagonale von links oben nach rechts unten.
-func getDiagonal1(board [][]string) []string {
-	var result []string
-	for i, _ := range board {
-		result = append(result, board[i][i])
-	}
-
-	return result
-}
-
-// Liefert die Diagonale von rechts oben nach links unten.
-func getDiagonal2(board [][]string) []string {
-	var result []string
-	l := len(board) - 1
-	for i, _ := range board {
-		result = append(result, board[i][l-i])
-	}
-
-	return result
-}
-
 // Nimmt das Board als Parameter und liefert true, wenn
 // es in einer Diagonale drei mal 'char' enthält.
 func checkDiagonals(board [][]string, char string) bool {
-	if checkList(getDiagonal1(board), char) {
+	if checkList(b.GetDiagonal1(board), char) {
 		return true
 	}
-	if checkList(getDiagonal2(board), char) {
+	if checkList(b.GetDiagonal2(board), char) {
 		return true
 	}
 
