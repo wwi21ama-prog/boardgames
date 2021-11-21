3ea9c086246f2ae84cd05d48e43b7760ad5d52bf
Reiner Hüchting <reiner.huechting@gmx.de>
2021-11-21 20:38:15 +0100

    Zugeingabe: Anzeige des Spielfelds und Prüfung der Eingabe hinzugefügt.

--- tictactoe/spielfeld.go
+++ tictactoe/spielfeld.go
@@ -11,31 +11,53 @@ func main() {
 // Abwechselnd Spieler X und O nach ihren Zügen fragen,
 // die Züge machen und auswerten, ob jemand gewonnen hat.
 func mainLoop(board [][]string) {
-	currentPlayer := "X"
+	currentPlayer := "O"
+	// Anmerkung: Wir starten mit Spieler O, weil eigentlich spieler X starten soll.
+	// Unten in der Schleife wird als erstes der Spieler gewechselt.
 
 	// Solange der aktuelle Spieler nicht gewonnen hat...
 	for !checkWinner(board, currentPlayer) {
 
 		// Den Spieler wechseln
+		// Dies geschieht hier vor dem Zug, damit nach Ende der Schleife currentPlayer noch
+		// der Spieler ist, der zuletzt einen Zug gemacht hat.
+		if currentPlayer == "X" {
+			currentPlayer = "O"
+		} else {
+			currentPlayer = "X"
+		}
 
 		// Den aktuellen Spieler nach seinem Zug fragen und ihn ausführen.
 		move(board, currentPlayer)
-
 	}
 }
 
 // Fragt den Spieler mit dem Zeichen 'char' nach seinem Zug
 // (z.B. Zeile und Spalte) und führt ihn aus.
 func move(board [][]string, char string) {
+	printBoard(board)
 	fmt.Printf("Spieler %v, du bist dran.\n", char)
 
 	var row, col int
 
 	fmt.Print("Gib deine Zeile ein: ")
 	fmt.Scanln(&row)
+	if row < 0 || row > 2 {
+		fmt.Println("Die Zeile ist nicht gültig. Bitte noch einmal probieren.")
+		move(board, char)
+		return
+	}
 	fmt.Print("Gib deine Spalte ein: ")
 	fmt.Scanln(&col)
-
+	if col < 0 || col > 2 {
+		fmt.Println("Die Spalte ist nicht gültig. Bitte noch einmal probieren.")
+		move(board, char)
+		return
+	}
+	if board[row][col] != " " {
+		fmt.Println("Das Feld ist schon belegt. Bitte noch einmal probieren.")
+		move(board, char)
+	}
 	board[row][col] = char
 }
 
